daa891150c3e5c9bb330f460c5b0ded524b8f20e
zhou xiaojiang <zhouxiaojiang@startdt.com>
2018-03-05 17:02:03 +0800

    update: code comments internationalization

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-/*
- * 前端监控中心：搜集各种信息
+/*!
+ * Fe monitor center, collect anything if we need
  */
-const BASE_URL = 'http://localhost:3000/mc'
+const BASE_URL = 'https://mc.startdt.com/'
 
-// post提交数据
+// Submit Data with post
 function sendDataByPost (sendDataStr) {
   var xmlhttp = new XMLHttpRequest()
   xmlhttp.open('POST', BASE_URL, true)
   xmlhttp.send(sendDataStr)
 }
 
-// get提交数据
+// Submit Data with get
 function sendDataByGet (sendDataStr) {
   const img = new Image()
   img.src = `${BASE_URL}?${sendDataStr}?t=${Date.now()}`
 }
 
+/**
+ * collect api 
+ *
+ * @param {String} type monitor type
+ * @param {Object} callback collect data info
+ */
 function MonitorCenter (type = 'apiMonitor', params = {}) {
   const enCodeParams = encodeURIComponent(JSON.stringify({params}))
-  // 超过3000个字符，改用post上传数据
+  // if the character length more than 300, Change the request method into 'POST' 
   const sendDataStr = `type=${type}&params=${enCodeParams}`
   if (sendDataStr.length > 3000) {
     sendDataByPost(sendDataStr)
